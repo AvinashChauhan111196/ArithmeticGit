0fe193d08427bc33b617c9b90a23b20bd307ffee
Avinash <Avichauhan111196@gmail.com>
2021-12-22 14:27:33 +0530

    Added : Addition

---
+++ src/Arithmetic.java
@@ -0,0 +1,22 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a, b, addition, subtraction, multiplication, division;
+		
+		a = 20;
+		b = 10;
+		
+		addition = a + b;
+		subtraction = a - b;
+		multiplication = a * b;
+		division = a / b;
+		
+		System.out.println(" Addition is " + addition);
+		
+
+	}
+
+}
